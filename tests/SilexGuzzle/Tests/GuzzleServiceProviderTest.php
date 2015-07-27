bb068e15fe5e6443e4006db67b55d5988bd5b532
Jean-philippe Depigny <jdepigny.ext@orange.com>
2015-07-27 18:33:27 +0200

    add tests to silexGuzzleProvider

---
+++ tests/SilexGuzzle/Tests/GuzzleServiceProviderTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace SilexGuzzle\Tests\Extension;
+
+use Silex\Application;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use SilexGuzzle\GuzzleServiceProvider;
+
+class GuzzleServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        if (!class_exists('GuzzleHttp\\Client')) {
+            $this->markTestSkipped('Guzzle was not installed.');
+        }
+    }
+    
+    public function testRegister()
+    {
+        $app = new Application();
+        $app->register(new GuzzleServiceProvider());
+            
+        $app->get('/', function() use($app) {
+            $app['guzzle'];
+        });
+        $request = Request::create('/');
+        $app->handle($request);
+        
+        $this->assertInstanceOf('GuzzleHttp\Client', $app['guzzle']);
+    }
+
+    /**
+     * test to connecto httpBin
+     */
+    public function testConnectToApi()
+    {
+        $app = new Application();
+        $app->register(new GuzzleServiceProvider());
+
+        $app->get('/', function() use($app) {
+            $app['guzzle'];
+        });
+        $request = Request::create('/');
+        $app->handle($request);
+        $myRequest = $app['guzzle']->get('http://httpbin.org/');
+        $this->assertSame($myRequest->getStatusCode(),"200");
+    }
+
+
+    /**
+     * @Exception
+     */
+    public function testTimeoutApi()
+    {
+        $app = new Application();
+        $app->register(new GuzzleServiceProvider());
+
+        $app->get('/', function() use($app) {
+            $app['guzzle'];
+        });
+        $request = Request::create('/');
+        $app->handle($request);
+        $myRequest = $app['guzzle']->get('http://httpbin.org/delay/5');
+        $this->assertSame($myRequest->getStatusCode(),"200");
+    }
+}
