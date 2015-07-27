a04b5bfe94cd6072092545f9d31722306742b87b
Jean-Philippe Dépigny <jp.depigny@gmail.com>
2015-07-28 00:09:49 +0200

    add test for base_uri parameter

--- tests/SilexGuzzle/Tests/GuzzleServiceProviderTest.php
+++ tests/SilexGuzzle/Tests/GuzzleServiceProviderTest.php
@@ -64,4 +64,43 @@ class GuzzleServiceProviderTest extends \PHPUnit_Framework_TestCase
         $app->handle($request);
         $app['guzzle']->get('http://httpbin.org/delay/5');
     }
+
+
+    public function testSetTimeout()
+    {
+        $app = new Application();
+        $app->register(new GuzzleServiceProvider(), array(
+                'guzzle.timeout' => 3.14,
+            ));
+
+        $app->get('/', function() use($app) {
+                $app['guzzle'];
+            });
+        $request = Request::create('/');
+        $app->handle($request);
+        $config = $app['guzzle']->getConfig();
+        $this->assertArrayHasKey('timeout',$config);
+        $this->assertSame($config['timeout'], 3.14);
+        $this->assertEquals($config['timeout'], 3.14);
+    }
+
+    public function testSetBaseUri()
+    {
+        $app = new Application();
+        $app->register(new GuzzleServiceProvider(), array(
+                'guzzle.base_uri' => 'http://httpbin.org/',
+            ));
+
+        $app->get('/', function() use($app) {
+                $app['guzzle'];
+            });
+        $request = Request::create('/');
+        $app->handle($request);
+        $config = $app['guzzle']->getConfig();
+        $this->assertArrayHasKey('base_uri',$config);
+        $this->assertEquals($config['base_uri'], 'http://httpbin.org/');
+        $myRequest = $app['guzzle']->get('/ip');
+        $this->assertObjectHasAttribute('origin', json_decode($myRequest->getBody()->getContents()));
+
+    }
 }
