6ebf219810ba0f0a1e08a06c0eba8faaacaf8f26
rebangm <jp.depigny@gmail.com>
2015-07-30 17:32:45 +0200

    add other testCase in example files

--- example/guzzleTestApi.php
+++ example/guzzleTestApi.php
@@ -5,7 +5,11 @@ require_once "../vendor/autoload.php";
 $app = new Silex\Application();
 $app['debug'] = true;
 $app['guzzle.timeout'] = 1.0;
-$app->register(new SilexGuzzle\GuzzleServiceProvider());
+$app->register(new SilexGuzzle\GuzzleServiceProvider(),array(
+    'guzzle.timeout' => 3.14,
+    'guzzle.request_options' =>
+        ['auth' => ['admin', 'admin']]
+));
 
 
 $app->get('/', function() use($app) {
@@ -13,6 +17,19 @@ $app->get('/', function() use($app) {
     $request = $app['guzzle']->get('http://httpbin.org/ip');
     return 'HttpBin API : "Status Code" "200" "' . $request->getStatusCode() . '"  '. $request->getBody()->getContents();
 });
+
+$app->get('/get', function() use($app) {
+
+    $request = $app['guzzle']->get('http://httpbin.org/get?q=foo');
+    return 'HttpBin API : "Status Code" "200" "' . $request->getStatusCode() . '"  '. $request->getBody()->getContents();
+});
+
+$app->get('/auth', function() use($app) {
+
+    $request = $app['guzzle']->get('http://httpbin.org/basic-auth/admin/admin');
+    return 'HttpBin API : "Status Code" "200" "' . $request->getStatusCode() . '"  '. $request->getBody()->getContents();
+});
+
 
 $app->get('/testTimeout', function() use($app) {
     try {
