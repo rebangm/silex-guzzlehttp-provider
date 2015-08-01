d6399a3102ede4b9a1962dcca1721dd397784eee
rebangm <jp.depigny@gmail.com>
2015-08-01 18:57:58 +0200

    add licence message

--- example/guzzleTestApi.php
+++ example/guzzleTestApi.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Silex Guzzle Provider.
+ *
+ * (c) Jean-Philippe Dépigny <jp.depigny@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 require_once "../vendor/autoload.php";
 
